445bb21f481c32445f4a14105d2280068d28da81
Ridwan <ridwanpr@outlook.com>
2025-06-01 04:07:25 +0700

    first commit

---
+++ app/Http/Controllers/WatchlistController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Watchlist;
+use App\Http\Requests\StoreWatchlistRequest;
+use App\Http\Requests\UpdateWatchlistRequest;
+
+class WatchlistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWatchlistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Watchlist $watchlist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWatchlistRequest $request, Watchlist $watchlist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Watchlist $watchlist)
+    {
+        //
+    }
+}
